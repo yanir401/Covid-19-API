279d9480c313a0f233be05606f3ac6b93d3aef60
יניר יניר <yanir401@gmail.com>
2022-05-13 23:52:26 +0300

    fix axios cdn

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,25 @@
-const proxy = "https://cors-anywhere.herokuapp.com/";
+// const proxy = "https://cors-anywhere.herokuapp.com/";
+
+// export async function getWorldwideCovidData() {
+//   try {
+//     const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
+//     return data.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// export async function getCountries() {
+//   try {
+//     const { data } = await axios.get(
+//       `${proxy}https://restcountries.herokuapp.com/api/v1`
+//     );
+//     return data;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+const proxy = "";
 
 export async function getWorldwideCovidData() {
   try {
