8d644b79db62f0e7c327ad3a5d4dc6a64ac3da56
יניר יניר <yanir401@gmail.com>
2022-05-14 20:41:20 +0300

    fix fetch api

--- js/fetch.js
+++ js/fetch.js
@@ -1,30 +1,8 @@
-// const proxy = "https://cors-anywhere.herokuapp.com/";
-
-// export async function getWorldwideCovidData() {
-//   try {
-//     const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
-//     return data.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// export async function getCountries() {
-//   try {
-//     const { data } = await axios.get(
-//       `${proxy}https://restcountries.herokuapp.com/api/v1`
-//     );
-//     return data;
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-const proxy = "";
+const proxy = "https://cors-anywhere.herokuapp.com/";
 
 export async function getWorldwideCovidData() {
   try {
     const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
-    console.log(data);
     return data.data;
   } catch (error) {
     console.error(error);
@@ -35,10 +13,32 @@ export async function getCountries() {
     const { data } = await axios.get(
       `${proxy}https://restcountries.herokuapp.com/api/v1`
     );
-    console.log(data);
-
     return data;
   } catch (error) {
     console.error(error.message);
   }
 }
+
+// const proxy = "";
+
+// export async function getWorldwideCovidData() {
+//   try {
+//     const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
+//     console.log(data);
+//     return data.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// export async function getCountries() {
+//   try {
+//     const { data } = await axios.get(
+//       `${proxy}https://restcountries.herokuapp.com/api/v1`
+//     );
+//     console.log(data);
+
+//     return data;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
