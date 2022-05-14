a5e8f26a45acac9a79ee5439ffb0fc158d2db7fd
יניר יניר <yanir401@gmail.com>
2022-05-14 20:45:39 +0300

    fix fetch api

--- js/fetch.js
+++ js/fetch.js
@@ -3,6 +3,7 @@ const proxy = "https://cors-anywhere.herokuapp.com/";
 export async function getWorldwideCovidData() {
   try {
     const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
+    console.log("http://corona-api.com/countries");
     console.log(data);
     return data.data;
   } catch (error) {
