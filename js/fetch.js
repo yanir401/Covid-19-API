d869fa6c9b2683e949d37271cf65980bc1e38442
יניר יניר <yanir401@gmail.com>
2022-05-15 00:15:49 +0300

    fix fetch api

--- js/fetch.js
+++ js/fetch.js
@@ -1,18 +1,8 @@
-const proxy = "http://api.allorigins.win/get?url=";
+const proxy = "https://api.allorigins.win/get?url=";
 
 export async function getWorldwideCovidData() {
-  // try {
-  //   const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
-  //   console.log("http://corona-api.com/countries");
-  //   console.log(data);
-  //   return data.data;
-  // } catch (error) {
-  //   console.error(error);
-  // }
   const res = await fetch(
-    `https://api.allorigins.win/get?url=${encodeURIComponent(
-      "http://corona-api.com/countries"
-    )}`
+    `${proxy}${encodeURIComponent("http://corona-api.com/countries")}`
   )
     .then((response) => {
       if (!response.ok) throw new Error("Network response was not ok.");
@@ -21,21 +11,12 @@ export async function getWorldwideCovidData() {
     .then((data) => {
       return JSON.parse(data.contents);
     });
-  console.log(res);
   return res.data;
 }
 
 export async function getCountries() {
-  // try {
-  //   const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
-  //   console.log("http://corona-api.com/countries");
-  //   console.log(data);
-  //   return data.data;
-  // } catch (error) {
-  //   console.error(error);
-  // }
   const res = await fetch(
-    `https://api.allorigins.win/get?url=${encodeURIComponent(
+    `${proxy}${encodeURIComponent(
       "https://restcountries.herokuapp.com/api/v1"
     )}`
   )
@@ -46,41 +27,6 @@ export async function getCountries() {
     .then((data) => {
       return JSON.parse(data.contents);
     });
-  console.log(res);
+
   return res;
 }
-// export async function getCountries() {
-//   try {
-//     const { data } = await axios.get(
-//       `${proxy}https://restcountries.herokuapp.com/api/v1`
-//     );
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-// const proxy = "";
-
-// export async function getWorldwideCovidData() {
-//   try {
-//     const { data } = await axios.get(`${proxy}http://corona-api.com/countries`);
-//     console.log(data);
-//     return data.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// export async function getCountries() {
-//   try {
-//     const { data } = await axios.get(
-//       `${proxy}https://restcountries.herokuapp.com/api/v1`
-//     );
-//     console.log(data);
-
-//     return data;
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
